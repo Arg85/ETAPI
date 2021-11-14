46c9b9db992021edfea6ba075e396bea61db5cb2
Anurag Yadav <anuragy554@gmail.com>
2021-11-15 00:21:40 +0530

    18/11/21

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
-const express=require('express')
+const express = require("express");
 const app = express();
-const userRoute=require('./Routes/userRoute')
+const datya = [{ name: "Anurag" ,dob:"109123012",course:"BE"}, { name: "Leesha",dob:"109123012",course:"BE" }, { name: "Rishika",dob:"109123012",course:"BE" }];
+const datya2 = [{ name: "Anurag" ,course:"BE"}, { name: "Leesha",course:"BE" }, { name: "Rishika",course:"BE" }];
+const userRoute = require("./Routes/userRoute");
 // app.get('/users',userRoute)
-app.use('/users',userRoute)
-app.listen(7000,()=>{
-console.log("server runnning on port 7000")
-})
+app.use(express.json())
+app.use("/users", userRoute);
+app.get("/RungtaStudents", (req, res) => {
+  console.log(req.body)
+  
+});
+app.get("/RungtaStudents/Short", (req, res) => {
+  res.status(404).send(datya2);
+});
+app.listen(7000, () => {
+  console.log("server runnning on port 7000");
+});
